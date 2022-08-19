532e2b584506cba409b74528dc08e244ed8e8d45
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-08-19 10:28:43 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { getContacts } from 'redux/contacts/contactOperations';
+import { getItems } from '../redux/contacts/contactsSelrctor';
+import s from '../components/App.module.css';
+
+function App() {
+  const contacts = useSelector(getItems);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.decor}>
+      <h1 className={s.title}>Phonebook📱</h1>
+      <ContactForm />
+      {contacts.length === 0 ? (
+        <h2 className={s.title}>
+          You have no records☝
+          <br />
+          Enter the number and name☝
+        </h2>
+      ) : (
+        <>
+          <h2 className={s.title}>Contacts: {contacts.length}</h2>
+          <Filter />
+          <ContactList />
+        </>
+      )}
     </div>
   );
-};
+}
+export default App;
